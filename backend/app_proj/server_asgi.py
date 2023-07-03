47861367e8ba318fef10698fbe7fae63028140e7
Your Name <you@example.com>
2023-07-02 20:56:38 -0400

    settings added

--- backend/app_proj/server_asgi.py
+++ backend/app_proj/server_asgi.py
@@ -6,8 +6,9 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
 from app_proj.routing import websocket_urls
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_proj.settings')    # needed in prod
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
